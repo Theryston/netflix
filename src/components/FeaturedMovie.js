026e26680463cd3c8df131d5e2570325c1e02101
Theryston <funktodo2@gmail.com>
2021-10-24 10:46:34 -0300

    '

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -5,6 +5,13 @@ import './FeaturedMovie.css';
 export default ({ item }) => {
   console.log(item);
 
+  const firstDate = new Date(item.first_air_date);
+  const genres = [];
+
+  for (let i in item.genres) {
+    genres.push(item.genres[i].name);
+  }
+
   return (
     <section className="featured" style={{
       backgroundSize: 'cover',
@@ -16,9 +23,25 @@ export default ({ item }) => {
           <div className="featured--name">{item.name}</div>
           <div className="featured--info">
             <div className="featured--points">{item.vote_average} pontos</div>
-            <div className="featured--year">2099</div>
-            <div className="featured--seasons">{item.number_of_seasons} temporadas</div>
+            <div className="featured--year">{firstDate.getFullYear()}</div>
+            <div className="featured--seasons">{item.number_of_seasons} temporada{item.number_of_seasons !== 1 ? 's' : ''}</div>
           </div>
+          <div className="featured--description">{item.overview}</div>
+          <div className="featured--buttons">
+            <a href="/" className="featured--watchButton">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-play-fill" viewBox="0 0 16 16">
+                <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z" />
+              </svg>
+              Assistir
+            </a>
+            <a href="/" className="featured--infoButton">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-lg" viewBox="0 0 16 16">
+                <path d="m9.708 6.075-3.024.379-.108.502.595.108c.387.093.464.232.38.619l-.975 4.577c-.255 1.183.14 1.74 1.067 1.74.72 0 1.554-.332 1.933-.789l.116-.549c-.263.232-.65.325-.905.325-.363 0-.494-.255-.402-.704l1.323-6.208Zm.091-2.755a1.32 1.32 0 1 1-2.64 0 1.32 1.32 0 0 1 2.64 0Z" />
+              </svg>
+              Mais informações
+            </a>
+          </div>
+          <div className="featured--genres"><strong>Gêneros:</strong> {genres.join(', ')}</div>
         </div>
       </div>
     </section>
